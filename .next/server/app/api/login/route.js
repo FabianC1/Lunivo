360a7b7c09ac147fda4696632796921dc622081a
Fabian Galasel <fabianelissa7@gmail.com>
2026-05-11 09:36:39 +0000

    started coding on gitpod
    Co-authored-by: Ona <no-reply@ona.com>

--- .next/server/app/api/login/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/login/route.js")
-R.c("server/chunks/_9754d585._.js")
-R.c("server/chunks/[root-of-the-server]__81ca0b1a._.js")
-R.c("server/chunks/[root-of-the-server]__24f8fcd9._.js")
-R.c("server/chunks/node_modules_next_f2da0d3e._.js")
-R.c("server/chunks/node_modules_bcryptjs_index_42ebb250.js")
-R.c("server/chunks/_next-internal_server_app_api_login_route_actions_a124d239.js")
-R.m(25586)
-module.exports=R.m(25586).exports
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2U8/1SNyLojNmAOOAb0HfsTJQGLzQr9+fuDq/HMWReI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
